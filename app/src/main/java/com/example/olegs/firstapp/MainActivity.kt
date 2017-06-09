e9acc65bf8c6f16c216d6bc93586fc193b854fbf
olegstabr <olegstabr@gmail.com>
2017-06-09 19:08:34 +0700

    update

--- app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.olegs.firstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloTextView = findViewById(R.id.textView)
+        if (helloTextView is TextView) {
+            val button = findViewById(R.id.button)
+            var i = 0
+            button.setOnClickListener(View.OnClickListener {
+                helloTextView.text = i++.toString()
+            })
+        }
     }
 }
