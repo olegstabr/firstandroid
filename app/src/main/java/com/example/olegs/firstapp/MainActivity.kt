e3a6f3334a2f4aa843fc4217aef90b4a855a1739
olegstabr <olegstabr@gmail.com>
2017-06-09 19:38:51 +0700

    update

--- app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.example.olegs.firstapp
 
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var helloTextView :TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloTextView = findViewById(R.id.textView)
-        if (helloTextView is TextView) {
-            val button = findViewById(R.id.button)
-            var i = 0
-            button.setOnClickListener(View.OnClickListener {
-                helloTextView.text = i++.toString()
-            })
+        helloTextView = findViewById(R.id.textView) as? TextView
+        val button = findViewById(R.id.button)
+        var i = 0
+        button.setOnClickListener(onClickListener)
+        helloTextView?.setOnClickListener(onClickListener)
+    }
+
+    open val onClickListener : OnClickListener = OnClickListener() {
+        v -> val id = v.id
+        Log.d("MyLogs", "Click Event")
+        when (id) {
+            R.id.textView -> {
+                helloTextView?.text = "TextView Clicked"
+                Toast.makeText(this, "Вы нажали на TextView", Toast.LENGTH_SHORT).show()
+            }
+            R.id.button -> {
+                helloTextView?.text = "Button Clicked"
+                Toast.makeText(this, "Вы нажали на Button", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
