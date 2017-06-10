efcfa454e78b4dd16f8a9b7e7d5987229094b193
olegstabr <olegstabr@gmail.com>
2017-06-10 18:56:15 +0700

    update

--- app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.olegs.firstapp
 import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
@@ -10,31 +12,20 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var helloTextView :TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        helloTextView = findViewById(R.id.textView) as? TextView
-//        val button = findViewById(R.id.button)
-//        var i = 0
-//        button.setOnClickListener(onClickListener)
-//        helloTextView?.setOnClickListener(onClickListener)
+        val actionButton = findViewById(R.id.main_floatbutton)
+        actionButton?.setOnClickListener(OnClickListener {
+            v -> v
+            val snackbar = Snackbar.make(v, "Own action", Snackbar.LENGTH_SHORT).setAction("OK", onSnackbarClickListener)
+            snackbar.show()
+
+        })
     }
 
-//    open val onClickListener : OnClickListener = OnClickListener() {
-//        v -> val id = v.id
-//        Log.d("MyLogs", "Click Event")
-//        when (id) {
-//            R.id.textView -> {
-//                helloTextView?.text = "TextView Clicked"
-//                Toast.makeText(this, "Вы нажали на TextView", Toast.LENGTH_SHORT).show()
-//            }
-//            R.id.button -> {
-//                helloTextView?.text = "Button Clicked"
-//                Toast.makeText(this, "Вы нажали на Button", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
+    open val onSnackbarClickListener : View.OnClickListener = OnClickListener {
+        Toast.makeText(applicationContext, "Good Job!", Toast.LENGTH_SHORT).show()
+    }
 }
