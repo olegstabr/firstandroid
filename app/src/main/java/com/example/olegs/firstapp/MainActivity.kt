d7c24245ddcf5bb346523bf6befdcdc14c5affef
olegstabr <olegstabr@gmail.com>
2017-06-10 20:38:24 +0700

    update

--- app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
@@ -6,12 +6,16 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    open val NOTIFY_ID = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,21 @@ class MainActivity : AppCompatActivity() {
             snackbar.show()
 
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        when (id) {
+            R.id.action_item1 -> Toast.makeText(applicationContext, "Вы создали уведомление", Toast.LENGTH_LONG).show()
+            R.id.action_item2 -> Toast.makeText(applicationContext, "Вы выбрали пункт 2", Toast.LENGTH_LONG).show()
+            R.id.action_item3 -> Toast.makeText(applicationContext, "Вы выбрали пункт 3", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     open val onSnackbarClickListener : View.OnClickListener = OnClickListener {
