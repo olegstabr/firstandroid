5658747d8187e1edbcd57427dab6bd187ec13ea1
olegstabr <olegstabr@gmail.com>
2017-06-10 17:16:27 +0700

    update

--- app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/MainActivity.kt
@@ -16,25 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        helloTextView = findViewById(R.id.textView) as? TextView
-        val button = findViewById(R.id.button)
-        var i = 0
-        button.setOnClickListener(onClickListener)
-        helloTextView?.setOnClickListener(onClickListener)
+//        helloTextView = findViewById(R.id.textView) as? TextView
+//        val button = findViewById(R.id.button)
+//        var i = 0
+//        button.setOnClickListener(onClickListener)
+//        helloTextView?.setOnClickListener(onClickListener)
     }
 
-    open val onClickListener : OnClickListener = OnClickListener() {
-        v -> val id = v.id
-        Log.d("MyLogs", "Click Event")
-        when (id) {
-            R.id.textView -> {
-                helloTextView?.text = "TextView Clicked"
-                Toast.makeText(this, "Вы нажали на TextView", Toast.LENGTH_SHORT).show()
-            }
-            R.id.button -> {
-                helloTextView?.text = "Button Clicked"
-                Toast.makeText(this, "Вы нажали на Button", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+//    open val onClickListener : OnClickListener = OnClickListener() {
+//        v -> val id = v.id
+//        Log.d("MyLogs", "Click Event")
+//        when (id) {
+//            R.id.textView -> {
+//                helloTextView?.text = "TextView Clicked"
+//                Toast.makeText(this, "Вы нажали на TextView", Toast.LENGTH_SHORT).show()
+//            }
+//            R.id.button -> {
+//                helloTextView?.text = "Button Clicked"
+//                Toast.makeText(this, "Вы нажали на Button", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 }
