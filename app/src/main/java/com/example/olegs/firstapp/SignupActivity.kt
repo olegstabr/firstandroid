81dab05895ae0a8bdf02f9a0eacb5b94bf9f2c80
olegs <olegstabr@gmail.com>
2017-07-20 20:04:40 +0700

    create SendLoginData class

--- app/src/main/java/com/example/olegs/firstapp/SignupActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/SignupActivity.kt
@@ -3,8 +3,8 @@ package com.example.olegs.firstapp
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.olegs.firstapp.Auth.BasicAuthRestTemplate
 import com.example.olegs.firstapp.Rest.User
@@ -15,7 +15,9 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
  */
 
 class SignupActivity : AppCompatActivity() {
-    open var i = 0
+    var loginText: EditText? = null
+    var passwordText: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
@@ -25,25 +27,24 @@ class SignupActivity : AppCompatActivity() {
 
     fun onCreateButtonClick() {
         val createButton = findViewById(R.id.btn_signup)
-        createButton.setOnClickListener(View.OnClickListener {
-            val loginText = findViewById(R.id.input_name) as EditText
-            val passwordText = findViewById(R.id.input_password) as EditText
-            val login = loginText.text
-            val password = passwordText.text
-            val httpRequestTask = HttpRequestTask()
-            httpRequestTask.count = i++;
-            httpRequestTask.execute()
+        createButton.setOnClickListener({
+            val sendLoginData = SendLoginData()
+            sendLoginData.execute()
         })
     }
 
-    inner class  HttpRequestTask : AsyncTask<Void, Void, User>() {
-        var count = 0;
+    inner class SendLoginData : AsyncTask<Void, Void, User>() {
         override fun doInBackground(vararg params: Void?): User? {
             try {
                 val url = "http://192.168.0.104:8080/user/"
+                loginText = findViewById(R.id.input_name) as EditText
+                passwordText = findViewById(R.id.input_password) as EditText
+                val login = loginText?.text.toString()
+                val password = passwordText?.text.toString()
                 val restTempalte = BasicAuthRestTemplate("bill", "abc123")
+
                 restTempalte.messageConverters.add(MappingJackson2HttpMessageConverter())
-                val user = restTempalte.postForObject(url, User(count, count.toString(), "CXCXCXC"), User::class.java)
+                val user = restTempalte.postForObject(url, User(login, password), User::class.java)
                 return user
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -52,7 +53,9 @@ class SignupActivity : AppCompatActivity() {
         }
 
         override fun onPostExecute(user: User?) {
-            Toast.makeText(applicationContext, "OKOKOK", Toast.LENGTH_SHORT).show()
+            loginText?.setText("", TextView.BufferType.EDITABLE)
+            passwordText?.setText("", TextView.BufferType.EDITABLE)
+            Toast.makeText(applicationContext, "Вы успешно зарегистрировались", Toast.LENGTH_SHORT).show()
         }
     }
 }
