f88bd231e413cd5331aa4788076f34334e3b31fb
Oleg <olegstabr@gmail.com>
2017-07-29 15:06:29 +0700

    add NoteEditActivity

--- app/src/main/java/com/example/olegs/firstapp/Activities/NotesListActivity.kt
+++ app/src/main/java/com/example/olegs/firstapp/Activities/NotesListActivity.kt
@@ -6,13 +6,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.olegs.firstapp.Auth.BasicAuthRestTemplate
 import com.example.olegs.firstapp.R
-import com.example.olegs.firstapp.Rest.Note
 import com.example.olegs.firstapp.Rest.NotesList
-import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 
 /**
@@ -22,7 +21,8 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 class NotesListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notes_list)
+        setContentView(R.layout.activity_note_list)
+        onListViewItemClick()
         val saveNote = SaveNote()
         saveNote.execute()
     }
@@ -47,6 +47,16 @@ class NotesListActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun onListViewItemClick() {
+        val listView = findViewById(R.id.list_view) as ListView
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(applicationContext, NoteEditActivity::class.java)
+            val item = listView.getItemAtPosition(position) as String
+            intent.putExtra("title", item)
+            startActivity(intent)
+        }
     }
 
     inner class SaveNote : AsyncTask<Void, Void, NotesList>() {
